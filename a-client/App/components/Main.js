bfca4be96fd439dafcdd3a39876983e2163a361b
Ignatius Ukwuoma <ignatius.ukwuoma@andela.com>
2017-09-09 10:30:37 +0100

    Create socialclub page

--- a-client/App/components/Main.js
+++ a-client/App/components/Main.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import HomePage from './HomePage';
-import Event from './events/event/Index';
-
+import SocialClubPage from './SocialClubPage';
 /**
  * Renders a component that contains all routes
  * @returns {object} jsx
  */
 function Main() {
   return (
-    <div>Don't need you.</div>
+    <Switch>
+      <Route path='/' component={SocialClubPage} />
+    </Switch>
   );
 }
 export default Main;
