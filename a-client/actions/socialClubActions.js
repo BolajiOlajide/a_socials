58856f98703ef3761579f6937700982e69be8211
Innocent <innocent.amadi@andela.com>
2017-09-10 09:11:14 +0100

    add getclubs action

--- a-client/actions/socialClubActions.js
+++ a-client/actions/socialClubActions.js
@@ -17,12 +17,33 @@ export function getClub(club_id) {
 
 export function joinClub(details) {
   return (dispatch) => {
-    return axios.post('/api/v1/join', details)
+    return axios.post('/api/v1/join/', details)
       .then((res) => {
         console.log('Res', res);
       })
       .catch(error => {
         throw error
       });
+  };
+}
+
+/**
+ * Social Club List Action Creator
+ */
+export function getClubs(socialClubs) {
+  return {
+    type: constants.GET_CLUBS,
+    clubs: socialClubs
+  }
+}
+
+export function getAllClubs() {
+  return (dispatch) => {
+    return axios.get('/api/v1/categories/').then(socialClubs => {
+      console.log(socialClubs);
+      dispatch(getClubs(socialClubs.data.results));
+    }).catch(error => {
+      throw error;
+    });
   };
 }
