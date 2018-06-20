ab84266569746bb4605315646844e6ba2351cc0e
Tomi Paul <tomipaul95@gmail.com>
2018-06-20 19:27:09 +0100

    update server scripts for use in both development and production

--- docker/wait_for_database.sh
+++ docker/wait_for_database.sh
@@ -7,7 +7,7 @@ import sys
 import psycopg2
 
 try:
-  conn = psycopg2.connect(dbname="$DB_NAME", user="$DB_USER", password="$DB_PASS", host="$DB_HOST")
+  conn = psycopg2.connect(dbname="$DB_NAME", user="$DB_USER", password="$DB_PASSWORD", host="$DB_HOST")
 except psycopg2.OperationalError:
     sys.exit(-1)
 sys.exit(0)
