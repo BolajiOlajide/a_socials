4e78c4adb3f5ab3f99872a17cece8e4f6f3d8f30
Oluwatosin Akinola <30419014+tosmak16@users.noreply.github.com>
2018-08-24 09:41:09 +0100

    #159892131 Create event load more component (#75)
    * feat(event-dashboard): fix some alignment and backgroud color issues
    
    * feat(event-dashboard): create event load more button component [Finishes #159985290]
    
    * feat(event-dashboard): Change eventspage.jsx to a class component [Finishes #159985290]

--- client/pages/Event/EventsPage.jsx
+++ client/pages/Event/EventsPage.jsx
@@ -5,33 +5,77 @@ import EventFilter from '../../components/filter/EventFilter';
 import EventCard from '../../components/cards/EventCard';
 import eventFixtureList from '../../fixtures/events';
 
+
 /**
  * @description Currently contains events page layout
  *
- * @function EventsPage
- *
- * @returns {JSX} Eventspage
+ * @class EventsPage
+ * @extends {React.Component}
  */
+class EventsPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { eventList: [] };
+  }
 
-const EventsPage = (props) => {
-  const { eventList } = props;
-  const events = eventList || eventFixtureList;
+  /**
+ * React Lifecycle hook
+ *
+ * @memberof EventsPage
+ * @returns {null}
+ */
+  componentDidMount() {
+    this.setState({ eventList: this.getEvents().length ? this.getEvents() : eventFixtureList });
+  }
 
-  const eventCards = events.map(eventListItem => (<EventCard key={eventListItem.id} {...eventListItem} />));
-  return (
-    <div className="event__container">
-      <div className="event__sidebar">
-        <EventFilter />
-        <Calendar />
+  /**
+  * @description Gets list of events
+   *
+   * @memberof EventsPage
+   */
+  getEvents = () => {
+    // Todo: Implement a call to get events
+    const eventList = [];
+    return eventList;
+  }
+
+  /**
+  * @description It loads more list of events
+  *
+   * @memberof EventsPage
+   */
+  loadMoreEvents = () => {
+    // Todo: Implement a call to load more events
+  }
+
+  /**
+  * @description It renders list of event card
+  *
+   * @memberof EventsPage
+   */
+  renderEventGallery = () => {
+    const { eventList } = this.state;
+    return eventList.map(eventItem => (<EventCard key={eventItem.id} {...eventItem} />));
+  }
+
+  render() {
+    return (
+      <div className="event__container">
+        <div className="event__sidebar">
+          <EventFilter />
+          <Calendar />
+        </div>
+        <div className="event__gallery">
+          {this.renderEventGallery()}
+        </div>
+        <div className="event__footer">
+          <button onClick={this.loadMoreEvents} type="button" className="btn-blue event__load-more-button">
+            Load more
+          </button>
+        </div>
       </div>
-      <div className="event__gallery">
-        {eventCards}
-      </div>
-      <div className="event__footer">
-        footer
-    </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default EventsPage;
