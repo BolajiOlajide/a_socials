04a73229e625c300c5dd45e6c9f6f8559b4633fb
Mayowa Makinde <mankinde53@yahoo.com>
2018-10-04 14:40:58 +0100

    add back parse_body function (#119)

--- server/graphql_schemas/views.py
+++ server/graphql_schemas/views.py
@@ -12,11 +12,18 @@ from graphql.error.located_error import GraphQLLocatedError
 
 class DRFAuthenticatedGraphQLView(FileUploadGraphQLView, GraphQLView):
 
+    def parse_body(self, request):
+        if isinstance(request, rest_framework.request.Request):
+            return request.data
+        return super(GraphQLView, self).parse_body(request)
+
     @classmethod
     def as_view(cls, *args, **kwargs):
         view = super(GraphQLView, cls).as_view(*args, **kwargs)
-        view = permission_classes(api_settings.DEFAULT_PERMISSION_CLASSES)(view)
-        view = authentication_classes(api_settings.DEFAULT_AUTHENTICATION_CLASSES)(view)
+        view = permission_classes(
+            api_settings.DEFAULT_PERMISSION_CLASSES)(view)
+        view = authentication_classes(
+            api_settings.DEFAULT_AUTHENTICATION_CLASSES)(view)
         view = api_view(['GET', 'POST'])(view)
         return view
 
