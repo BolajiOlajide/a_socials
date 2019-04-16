d5af1961454337b42edc2fc3e32629b0b2dcf97a
Kamara Deo <gkam1989@gmail.com>
2019-04-16 18:35:44 +0300

    #165215251 Strict RSVP functionality (#172)
    fix(RSVP): strict RSVP functionality
    
    - validate rsvp functionality
    - add check for timezone
    
    [Delivers #165215251]

--- server/graphql_schemas/attend/schema.py
+++ server/graphql_schemas/attend/schema.py
@@ -5,7 +5,10 @@ from graphql_relay import from_global_id
 from graphene_django.filter import DjangoFilterConnectionField
 from graphene_django.types import DjangoObjectType
 from django.core.exceptions import ObjectDoesNotExist
-
+from dateutil.parser import parse
+from graphql import GraphQLError
+from datetime import datetime
+from pytz import timezone
 
 from api.models import Attend, Event, AndelaUserProfile
 
@@ -30,14 +33,23 @@ class AttendEvent(relay.ClientIDMutation):
         status = input.get('status')
         db_event_id = from_global_id(event_id)[1]
         event = Event.objects.get(id=db_event_id)
+        event_start_date = parse(event.start_date)
+        event_tz = timezone(event.timezone)
+        if (event_start_date.tzinfo is None):
+            event_start_date = event_start_date.replace(tzinfo=event_tz)
+        today = datetime.now(event_tz)
         user = info.context.user
         andela_user_profile = AndelaUserProfile.objects.get(
             user_id=user.id)
         try:
-            user_attendance = Attend.objects.get(
-                user=andela_user_profile, event=event)
-            user_attendance.status = status
-            user_attendance.save()
+            if today < event_start_date:
+                user_attendance = Attend.objects.get(
+                    user=andela_user_profile, event=event)
+                user_attendance.status = status
+                user_attendance.save()
+            else:
+                raise GraphQLError(
+                    "The event is no longer available")
 
         except ObjectDoesNotExist:
             user_attendance = Attend.objects.create(
