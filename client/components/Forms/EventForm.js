48b2dcbb0272532be0268480bbabd82c43610ba2
Olusola Oguntimehin <30433120+olusoladavid@users.noreply.github.com>
2019-05-02 15:15:37 +0100

    fix(validation): fix event form validation (#175)
    - fix dynamic time field
    
    [Finishes #165767221]

--- client/components/Forms/EventForm.js
+++ client/components/Forms/EventForm.js
@@ -161,7 +161,7 @@ class EventForm extends Component {
     const errorFields = Object.keys(errors);
 
     errorFields.forEach((field) => {
-      if ((field !== 'time' && field !== 'imgUrl') && formData[field].trim().length === 0) {
+      if (typeof formData[field] === 'string' && !formData[field].trim().length) {
         errors[field].hasError = true;
       } else {
         errors[field].hasError = false;
