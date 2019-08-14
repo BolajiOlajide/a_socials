c7b6f0228067a909c258a5d2b31aac37a6c71449
Hammed Noibi <hnobi08@yahoo.com>
2019-08-14 16:26:45 +0100

    bug(events): fix delete event (#248)
    - disable cache in apolo client
        - add default option apollo instance
    
        [Fixes #167170876]

--- client/client.js
+++ client/client.js
@@ -1,8 +1,4 @@
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloLink,
-} from 'apollo-boost';
+import { ApolloClient, InMemoryCache, ApolloLink } from 'apollo-boost';
 
 import { createUploadLink } from 'apollo-upload-client';
 
@@ -11,17 +7,27 @@ const cache = new InMemoryCache();
 const uploadLink = createUploadLink({ uri: process.env.SERVER_API_BASE_URL });
 
 const middlewareAuthLink = new ApolloLink((operation, forward) => {
-  // The token should be gotten from the redux store or which ever location it is stored at login
-  const token = localStorage.getItem('token');
-  operation.setContext({ headers: { Token: token } });
-  return forward(operation);
+	// The token should be gotten from the redux store or which ever location it is stored at login
+	const token = localStorage.getItem('token');
+	operation.setContext({ headers: { Token: token } });
+	return forward(operation);
 });
 
 const httpLinkWithAuthToken = middlewareAuthLink.concat(uploadLink);
 
+const defaultCacheOptions = {
+  watchQuery: {
+    fetchPolicy: 'no-cache',
+  },
+  query: {
+    fetchPolicy: 'no-cache',
+  },
+}
+
 const Client = new ApolloClient({
-  link: httpLinkWithAuthToken,
+	link: httpLinkWithAuthToken,
   cache,
+  defaultOptions: defaultCacheOptions
 });
 
 export default Client;
